403df8eb70bfd9bc3701de64c4f03d4e13c7805e
LeoSuave25 <suaverdez90@gmail.com>
2023-05-13 16:15:32 +0800

    Pseudocode done

---
+++ tvclass.py
@@ -0,0 +1,16 @@
+'''LEOJ M. SUAVERDEZ OBJECT ORIENTED TV ASSIGNMENT'''
+# Create class named TV
+# Self Attributes for TV class which is channel, volume level, power
+# Turn on tv
+# Turn off tv
+# Get channel
+# Set Channel
+# Get Volume
+# Set Volume
+# Channel Up
+# Channel Down
+# Volume Up
+# Volume Down
+# Create two objects TV1 and TV2
+# TV1 = channel 30 and volume level 3
+# TV2 = channel 3 and volume level 2
