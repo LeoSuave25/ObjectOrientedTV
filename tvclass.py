ad831a1ef5c7136954351f09d256d7688b3f492a
LeoSuave25 <suaverdez90@gmail.com>
2023-05-13 20:51:09 +0800

    Added Turn off conditions to every method

--- tvclass.py
+++ tvclass.py
@@ -2,7 +2,11 @@
 # Create class named TV
 class TV:
 # Self Attributes for TV class which is channel, volume level, power
-    def __init__(self, channel, volume_level, power):
+    def __init__(self, channel=1, volume_level=1, power=False):
+        if not 1 <= channel <= 120:
+            raise ValueError("Channel must be between 1 and 120.")
+        if not 0 <= volume_level <= 7:
+            raise ValueError("Volume level must be between 0 and 7.")
         self.channel = channel
         self.volume_level = volume_level
         self.power = power
@@ -14,46 +18,67 @@ class TV:
         self.power = False
 # Get channel
     def getChannel(self):
-        print (f"Your Current Channel is {self.channel}")
+        if self.power:
+            print (f"Your Current Channel is {self.channel}")
+        else:
+            print("The TV is currently turned off.")
 # Set Channel
     def setChannel(self,newchannel):
-        if 1<newchannel<=120:
-            self.channel = newchannel
+        if self.power:
+            if 1<newchannel<=120:
+                self.channel = newchannel
+            else:
+                print(f"The new set channel ({newchannel}) is out of range\nYou are back to Channel({self.channel})")
         else:
-            print(f"The new set channel ({newchannel}) is out of range\nYou are back to Channel({self.channel})")
+            print("The TV is currently turned off.")
 # Get Volume
     def getVolume(self):
-        print (f"The Volume level is {self.volume_level}")
+        if self.power:
+            print (f"The Volume level is {self.volume_level}")
+        else:
+            print("The TV is currently turned off.")
 # Set Volume
     def setVolume(self,newvolume):
-        if 0<=newvolume<=7:
-            self.volume_level = newvolume
+        if self.power:
+            if 0<=newvolume<=7:
+                self.volume_level = newvolume
+            else:
+                print(f"The new set volume ({newvolume}) is out of range\nYou are back to Volume({self.volume_level})")
         else:
-            print(f"The new set volume ({newvolume}) is out of range\nYou are back to Volume({self.volume_level})")
+            print("The TV is currently turned off.")
 # Channel Up
     def channelUp(self):
-        if self.channel == 120:
-            print(f"You are currently at the end of the channel list which is ({self.channel})")
+        if self.power:
+            if self.channel == 120:
+                print(f"You are currently at the end of the channel list which is ({self.channel})")
+            else:
+                self.channel = int(self.channel) + 1 
         else:
-            self.channel = int(self.channel) + 1 
+            print("The TV is currently turned off.")
 # Channel Down
     def channelDown(self):
-        if self.channel == 1:
-            print(f"You are currently at channel ({self.channel}) and there is no channel 0")
+        if self.power:
+            if self.channel == 1:
+                print(f"You are currently at channel ({self.channel}) and there is no channel 0")
+            else:
+                self.channel = int(self.channel) - 1
         else:
-            self.channel = int(self.channel) - 1
+            print("The TV is currently turned off.")
 # Volume Up
     def volumeUp(self):
-        if self.volume_level == 7:
-            print("You are currently at the highest volume level")
+        if self.power:
+            if self.volume_level == 7:
+                print("You are currently at the highest volume level")
+            else:
+                self.volume_level = int(self.volume_level) + 1
         else:
-            self.volume_level = int(self.volume_level) + 1
+            print("The TV is currently turned off.")
 # Volume Down
     def volumeDown(self):
-        if self.volume_level == 0:
-            print("You are currently at the lowest volume level")
+        if self.power:
+            if self.volume_level == 0:
+                print("You are currently at the lowest volume level")
+            else:
+                self.volume_level = int(self.volume_level) - 1
         else:
-            self.volume_level = int(self.volume_level) - 1
-# Create two objects TV1 and TV2
-# TV1 = channel 30 and volume level 3
-# TV2 = channel 3 and volume level 2
+            print("The TV is currently turned off.")
